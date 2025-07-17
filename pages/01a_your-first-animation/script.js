9ea9976efec1b555af067a30029c3f1ea1eec1c5
Rifate nur shawn <33421686+Rifate-nur-shawn@users.noreply.github.com>
2025-07-17 20:44:08 +0600

    first commit

---
+++ pages/01a_your-first-animation/script.js
@@ -0,0 +1,20 @@
+gsap.to('.card',{
+    x:-100,
+    y:100,
+    yoyo: true,
+    opacity: 1,
+    scale: 0.5,
+    duration:1,
+    onComplete:() => {
+        gsap.to('.card',{
+            x:100 ,
+            y:-100,
+            repeat:-1,
+            yoyo: true,
+            duration: 1,
+            opacity: 1,
+            
+
+    })
+}
+})
